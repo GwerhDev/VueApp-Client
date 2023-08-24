fb5609fea4f2c68de941e03f36184c3c9e8533a5
Lalo Freak <lalofreak.dev@gmail.com>
2023-08-24 14:59:45 -0400

    first commit

---
+++ src/pages/LandingPage.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+defineProps<{ msg: string }>()
+
+</script>
+
+<template>
+  <h1>Prueba técnica</h1>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
