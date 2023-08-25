569dd8b14ec499c6c969af7a0f66d2d578965faa
Lalo Freak <lalofreak.dev@gmail.com>
2023-08-24 22:18:00 -0400

    get items by id action

--- src/app/components/Info/Info.component.vue
+++ src/app/components/Info/Info.component.vue
@@ -1,25 +1,15 @@
 <style scoped lang="scss" src="./Info.component.scss"/>
 <script setup lang="ts">
-  import { useStore } from '../../../middlewares/pinia/store';
   import { useRouter } from 'vue-router';
-
-  const store: any = useStore();
   const router: any = useRouter();
-  let isDataLoaded = false;
 
   const { id } = router.currentRoute._value.params;
-  await store.getItemsById(id)
-  console.log(store.details.title)
+  console.log(id)
+
 </script>
 
 <template>
   <main class="main-container">
-    <div v-if="isDataLoaded">
-      <h1>{{ store.details.title }}</h1>
-      <p>{{ store.details.body }}</p>
-    </div>
-    <div v-else>
-      <p>Cargando información...</p>
-    </div>
+    <h1>Detalles</h1>
   </main>
 </template>
--- src/app/pages/DetailsPage.vue
+++ src/app/pages/DetailsPage.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
 import BackButton from '../components/Buttons/Back/BackButton.component.vue';
 import Info from '../components/Info/Info.component.vue';
+import NavBar from '../components/NavBar/NavBar.component.vue';
 
 </script>
 
 <template>
-  <h1>Detalles</h1>
+  <NavBar/>
   <Info/>
   <BackButton/>
 </template>
