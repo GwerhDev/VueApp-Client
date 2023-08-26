f810f54c19146420456a8e7d0bd7ef52d898c784
Lalo Freak <lalofreak.dev@gmail.com>
2023-08-25 20:37:30 -0400

    styles

--- src/app/components/Presentation/Presentation.component.vue
+++ src/app/components/Presentation/Presentation.component.vue
@@ -7,27 +7,13 @@
   <div class="mb-4">
     <span>Tecnologías: <i>VueJs, Pinia, Axios & TypeScript</i></span>
     <div class="mt-1 logo-container">
-      <ul class="fd-col d-flex just-cent align-cent">
-        <img src="../../../assets/svg/vue.svg" height="45" alt="">
-      </ul>
-      
-      <ul class="fd-col d-flex just-cent align-cent">
-        <img src="../../../assets/svg/pinia.svg" height="45" alt="">
-      </ul>
-
-      <ul class="fd-col d-flex just-cent align-cent">
-        <img src="../../../assets/svg/axios.svg" height="45" alt="">
-      </ul>
-      <ul class="fd-col d-flex just-cent align-cent">
-        <img src="../../../assets/svg/typescript.svg" height="45" alt="">
-      </ul>
+      <img src="../../../assets/svg/vue.svg" height="45" alt="">
+      <img src="../../../assets/svg/pinia.svg" height="45" alt="">
+      <img src="../../../assets/svg/axios.svg" height="45" alt="">
+      <img src="../../../assets/svg/typescript.svg" height="45" alt="">
     </div>
   </div>
 </template>
 
-<style scoped>
-  .logo-container {
-    display: flex;
-    justify-content: space-evenly;
-  }
+<style scoped lang="scss" src="./Presentation.component.scss">
 </style>
